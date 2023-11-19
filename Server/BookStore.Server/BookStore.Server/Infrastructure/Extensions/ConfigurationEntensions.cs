064cdb4f2f98240af5a65e4af6df851d9443140d
DeniPanov <inedvonap@gmail.com>
2023-11-19 16:23:38 +0200

    Clean up Program.cs

---
+++ Server/BookStore.Server/BookStore.Server/Infrastructure/Extensions/ConfigurationEntensions.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore.Server.Infrastructure.Extensions
+{
+    public static class ConfigurationEntensions
+    {
+        public static AppSettings GetAppSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            var appSettingsConfig = configuration.GetSection("ApplicationSettings");
+            services.Configure<AppSettings>(appSettingsConfig);
+
+            return appSettingsConfig.Get<AppSettings>();
+        }
+    }
+}
---
+++ Server/BookStore.Server/BookStore.Server/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using BookStore.Server.Data;
+using BookStore.Server.Data.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BookStore.Server.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 1;
+            })
+            .AddEntityFrameworkStores<BookStoreDbContext>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, AppSettings appSettings)
+        {
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+        }
+    }
+}
